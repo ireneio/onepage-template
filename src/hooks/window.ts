da7fa584960c9f12bb7dbec27dc097ff0f0c91ab
irene <keiko15678@gmail.com>
2022-06-12 00:13:03 +0800

    lint

--- src/hooks/window.ts
+++ src/hooks/window.ts
@@ -1,17 +1,27 @@
+import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 
 export const useWindowWidth = () => {
   const [windowWidth, setWindowWidth] = useState(0);
+  const router = useRouter();
 
   useEffect(() => {
     const cb = () => {
       setWindowWidth(window.innerWidth);
     };
+    window.addEventListener('resize', cb);
     window.addEventListener('load', cb);
     return () => {
+      window.removeEventListener('resize', cb);
       window.removeEventListener('load', cb);
     };
   });
+
+  useEffect(() => {
+    if (window) {
+      setWindowWidth(window.innerWidth);
+    }
+  }, [router.pathname]);
 
   return windowWidth;
 };
